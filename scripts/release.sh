63d925b2981860058c227d0aa9f88e64419df780
Alex Rhea <alex.rhea@gmail.com>
2016-07-12 19:17:39 -0400

    fixes for memcached and added scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,3 +7,9 @@ docker build -f ../php/Dockerfile5 -t arhea/laravel:5 ../php
 
 docker build -f ../php/Dockerfile7 -t arhea/laravel:7 ../php
 docker tag arhea/laravel:7  arhea/laravel:latest
+
+docker push arhea/laravel-nginx:1.10
+docker push arhea/laravel-nginx:latest
+docker push arhea/laravel:5
+docker push arhea/laravel:7
+docker push arhea/laravel:latest
